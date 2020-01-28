9906c4188b1dd2e5ddd3e3ad02ceff07e720fd60
26148867 <26148867@UNA009-L103-003.serlab.intra>
2020-01-28 20:51:59 -0300

    commit 2

--- src/br/atenta/bean/Dependente.java
+++ src/br/atenta/bean/Dependente.java
@@ -3,10 +3,18 @@ package br.atenta.bean;
 public class Dependente extends Pessoa{
 
 	private int matriculaDependente;
+	private String parentesco;
+	
+	
+	
+	public String getParentesco() {
+		return parentesco;
+	}
 
-	
-	
-	
+	public void setParentesco(String parentesco) {
+		this.parentesco = parentesco;
+	}
+
 	public int getMatriculaDependente() {
 		return matriculaDependente;
 	}
